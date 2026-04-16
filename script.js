c341b260a11a3e95973e39f64632ee41d133fb97
jesseburke-04 <jesseburke04@gmail.com>
2026-04-16 10:03:37 -0400

    I think I'm finished the Javascript and HTML file.

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+// This si the Javascript file for the Final Exam
+// Calling the form and adding an event listener for when the form is submitted
 document.getElementById("Form").addEventListener("submit", function(event) {
+    // Prevent the default form submission behavior
   event.preventDefault();
-
+//create varibales for the JavaScript form
   var name = document.getElementById("name").value.trim();
-  var confirmation = document.getElementById("thankyouMessage");
+  var thankyouMessage = document.getElementById("thankyouMessage");
 
-  confirmation.textContent = "Thank you, " + name + "! You have registered.";
-  confirmation.style.display = "block";
+  thankyouMessage.textContent = "Thank you, " + name + "! You have registered.";
 });
